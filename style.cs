3fd2cb0426a0f78320c04dcb73388e4b86254786
SWARA492 <75374368+SWARA492@users.noreply.github.com>
2022-07-02 14:11:02 +0530

    Add files via upload

---
+++ style.cs
@@ -0,0 +1,24 @@
+
+.heading
+{
+font-size: 23px;
+width: 100%;
+margin: 0px;
+}
+
+#status
+{
+font-size: 18px;
+}
+body
+{
+    background-size:cover;
+}
+canvas{
+    border-radius: 40px;
+    border-width: 20px;
+    border-color: black;
+    border-style: ridge;
+    margin-top: 30px;
+    margin-right: 100px;
+}
